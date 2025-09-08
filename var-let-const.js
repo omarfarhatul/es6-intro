c0c0584940e2b7390bb9e4cbb786d54447e324c9
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-08 21:48:08 +0600

    const let array discuss

--- var-let-const.js
+++ var-let-const.js
@@ -5,3 +5,15 @@
 const money=25;
 const rich=money+25;
 console.log(rich);
+
+
+let count=0;
+count=count+15;
+console.log(count);
+
+
+// numbers of array
+const numbers=[35, 23, 75, 2, 4, 77, 32];
+numbers[3]=46;
+numbers.push(4, 7, 9);
+console.log(numbers);
