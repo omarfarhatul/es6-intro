d4784347ba79220fc17474d3200c9ad8a02d9190
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-10 23:50:46 +0600

    Add template of backtick

---
+++ template.js
@@ -0,0 +1,22 @@
+const first='Omar'
+const last='Farhatul'
+const name=first+' '+last
+console.log(name);
+
+const a=5;
+const b=4;
+const result='The sum of '+a+' and '+b+' is '+(a+b);
+console.log(result);
+
+// another formate
+const math=`The sum of ${a} and ${b} is ${a+b}`
+console.log(math);
+
+const email='Hi Jhone \n' + 
+'Cena'
+console.log(email);
+
+// backtick use
+const backtick=`Jhon Cena
+How are you?`
+console.log(backtick);
