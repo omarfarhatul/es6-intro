55fc6e46f73781b7f49cb3d9f7562ee80a8bad63
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-12 10:18:32 +0600

    Update

--- spread.js
+++ spread.js
@@ -11,3 +11,16 @@ const nums2=nums;
 nums2.push(23);
 console.log(nums);
 console.log(nums2);
+
+// another type
+const friend1=[3, 1, 6, 12, 7, 4];
+const bondhu=friend1;
+const dosto=[...friend1];
+console.log(dosto);
+friend1.push(45);
+console.log(dosto);
+console.log(friend1);
+
+// advanced: add extra element
+const n1=[...friend1, 777];
+console.log(n1);
